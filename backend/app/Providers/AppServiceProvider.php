25893764714818a3a975a3defb43c1909150e2fb
MuhammadSabri1306 <muhammadsabri1306@gmail.com>
2026-07-24 17:22:02 +0800

    update backend:change the rendered json for NotFoundHttpException, create isDebugDisabled macro for Request

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,5 +22,12 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Route::pattern('id', '[0-9]+');
+
+        Request::macro('isDebugDisabled', function (): bool {
+            if (!config('app.debug')) return true;
+
+            $hValue = $this->header('X-Disable-Debug');
+            return $hValue && in_array(strtolower($hValue), ['true', '1'], true);
+        });
     }
 }
--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -28,7 +29,16 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        $exceptions->shouldRenderJsonWhen(
-            fn (Request $request) => $request->is('api/*'),
-        );
+
+        $exceptions->shouldRenderJsonWhen(fn (Request $request) => $request->is('api/*'));
+
+        $exceptions->render(function (NotFoundHttpException $e, Request $request) {
+            if ($request->isDebugDisabled() && $request->is('api/*')) {
+                return response()->json([
+                    'message' => 'No resources found.',
+                    'data' => null,
+                ], 404);
+            }
+        });
+
     })->create();
